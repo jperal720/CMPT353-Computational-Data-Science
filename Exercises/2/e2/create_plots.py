963199cd8e9da6542fd31db599c504b8fc95ac51
Jonathan <jperalgo@sfu.ca>
2021-09-24 15:26:55 -0700

    ✅Completed Wikipedia Exercise.

--- Exercises/2/e2/create_plots.py
+++ Exercises/2/e2/create_plots.py
@@ -4,15 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from natsort import index_natsorted
 
-# filename1 = sys.argv[1]
-# filename2 = sys.argv[2]
+filename1 = sys.argv[1]
+filename2 = sys.argv[2]
 
-filename_test_1 = 'pagecounts-20190509-120000.txt'
-filename_test_2 = 'pagecounts-20190509-130000.txt'
-
-df1 = pd.read_csv(filename_test_1, sep=' ', header=None, index_col=1,
+df1 = pd.read_csv(filename1, sep=' ', header=None, index_col=1,
             names=['lang', 'page', 'views', 'bytes'])
-df2 = pd.read_csv(filename_test_2, sep=' ', header=None, index_col=1,
+df2 = pd.read_csv(filename2, sep=' ', header=None, index_col=1,
             names=['lang', 'page', 'views2', 'bytes'])
 
 
@@ -25,12 +22,11 @@ print(df3)
 
 df1 = df1.sort_values(by=['views'], ascending=False)
 
-# print(df1)
-
-
 plt.figure(figsize=(10,5)) #Change the size to something sensible
 plt.subplot(1, 2, 1) #Subplots in 1 row, 2 columns, select the first
 plt.plot(df1['views'].values) #build plot 1
 plt.subplot(1, 2, 2) #Subplots in 1 row, 2 columns, select the second
 plt.scatter(df3['views'], df3['views2']) #build plot 2
-plt.show()
+plt.xscale('log') #Scaling x axis
+plt.yscale('log') #Scaling y axis
+plt.savefig('wikipedia.png')
