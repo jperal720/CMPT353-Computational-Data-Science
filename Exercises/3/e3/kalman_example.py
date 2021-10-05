00858e1dccf0a2d652a7701a7ac2f2c51ee7a3a7
Jonathan <jperalgo@sfu.ca>
2021-10-05 13:45:14 -0700

    ✅Added crappy Kalman Filter in Exercise 3

--- Exercises/3/e3/kalman_example.py
+++ Exercises/3/e3/kalman_example.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
-import pykalman as pyk
+from statsmodels.nonparametric.smoothers_lowess import lowess
+from pykalman import KalmanFilter
 import pandas as pd
 import numpy as np
 
@@ -11,7 +12,31 @@ observations = pd.DataFrame()
 observations['sin'] = (np.sin(input_range)) + np.random.normal(0, 0.5, n_samples)
 observations['cos'] = (np.cos(input_range)) + np.random.normal(0, 0.25, n_samples)
 
+#LOESS Smoothing
+loess_smoothed = lowess(observations['sin'], input_range, 0.4)
+
+#Kalman Smoothing
+initial_value_guess = observations.iloc[0]
+observation_covariance = np.diag([0.5, 0.25]) ** 2
+
+delta_t = 2 * np.pi / n_samples
+transition_matrix = [[1, delta_t] , [-1 * delta_t, 1]]
+transition_covariance = np.diag([0.2, 0.2]) ** 2
+
+
+kf = KalmanFilter(
+    initial_state_mean=initial_value_guess,
+    initial_state_covariance=observation_covariance,
+    observation_covariance=observation_covariance,
+    transition_covariance=transition_covariance,
+    transition_matrices=transition_matrix
+)
+pred_state, state_cov = kf.smooth(observations)
+
+
 #Plotting
 plt.figure(figsize=(12, 4))
 plt.plot(input_range, observations['sin'], 'b.', alpha=0.5)
+# plt.plot(input_range, loess_smoothed[:, 1], 'r-', linewidth=3)
+plt.plot(input_range, pred_state[:, 0], 'r-', linewidth=3)
 plt.show()
--- Exercises/3/e3/smooth_temperature.py
+++ Exercises/3/e3/smooth_temperature.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 from statsmodels.nonparametric.smoothers_lowess import lowess
 import pandas as pd
 import numpy as np
-import pykalman as pyk
+from pykalman import KalmanFilter
 import sys
 
 dataset_location = 'sysinfo.csv'
@@ -23,16 +23,26 @@ kalman_data = cpu_data[['temperature', 'cpu_percent', 'sys_load_1', 'fan_rpm']]
 print(kalman_data)
 
 initial_state = kalman_data.iloc[0]
-observation_covariance = np.diag([0, 0, 0, 0]) ** 2 # TODO: shouldn't be zero
-transition_covariance = np.diag([0, 0, 0, 0]) ** 2 # TODO: shouldn't be zero
-transition = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]] # TODO: shouldn't (all) be zero
+observation_covariance = np.diag([0.5, 0.25, 0.5, 0.25]) ** 2 # TODO: shouldn't be zero
+transition_covariance = np.diag([0.2, 0.1, 0.2, 0.2]) ** 2 # TODO: shouldn't be zero
+transition = [[0.97,0.5,0.2,-0.001], [0.1,0.4,2.2,0], [0,0,0.95,0], [0,0,0,1]] # TODO: shouldn't (all) be zero
 
-print(initial_state)
+kf = KalmanFilter(
+    initial_state_mean=initial_state,
+    initial_state_covariance=observation_covariance,
+    observation_covariance=observation_covariance,
+    transition_covariance=transition_covariance,
+    transition_matrices=transition
+)
+pred_state, state_cov = kf.smooth(kalman_data)
+
+print(transition)
 
 
 
 plt.figure(figsize=(12, 4))
 plt.plot(cpu_data['timestamp'], cpu_data['temperature'], 'b.', alpha=0.5)
 plt.plot(cpu_data['timestamp'], loess_smoothed[:, 1], 'r-')
-# plt.show() # maybe easier for testing
-plt.savefig('cpu.svg') # for final submission
+plt.plot(cpu_data['timestamp'], pred_state[:, 0], 'r-')
+plt.show() # maybe easier for testing
+# plt.savefig('cpu.svg') # for final submission
