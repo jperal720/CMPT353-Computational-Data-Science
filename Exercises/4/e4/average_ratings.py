7886eb9a09c95f212aed7a7a498f65cb60c458ed
Jonathan <jperalgo@sfu.ca>
2021-10-08 19:20:01 -0700

    ✅Finished first part

--- Exercises/4/e4/average_ratings.py
+++ Exercises/4/e4/average_ratings.py
@@ -3,12 +3,18 @@ import numpy as np
 import difflib
 import re
 
-movie_list = pd.read_csv('movie_list.txt', names=['Movies'], delimiter="\n")
+movie_list = pd.read_csv('movie_list.txt', names=['movies'], delimiter="\n")
 movie_ratings_raw = pd.read_csv('movie_ratings.csv')
 
-movie_list = movie_list.set_index('Movies')
-movie_list['Num. of ratings'] = 'NaN'
-movie_list['Sum of ratings'] = 'NaN'
+movie_ratings_raw['title'] = movie_ratings_raw.apply(lambda x: difflib.get_close_matches(x['title'], movie_list['movies'], n=1), axis=1)
+print(movie_ratings_raw['title'][3] == [])
+movie_ratings_raw.drop(movie_ratings_raw[movie_ratings_raw['title'].astype(bool) != True].index, inplace=True)
 
-# print(movie_list)
-print(movie_list.loc['A Time to Kill'])
+# movie_ratings_raw['num_of_ratings'] = 1
+movie_ratings_raw['rating'] = movie_ratings_raw['rating'].astype(int)
+movie_ratings_raw['title'] = movie_ratings_raw['title'].astype(str)
+
+print(movie_ratings_raw.groupby(['title']).agg(
+    {'rating': ['mean']}
+).reset_index().round(2))
+
