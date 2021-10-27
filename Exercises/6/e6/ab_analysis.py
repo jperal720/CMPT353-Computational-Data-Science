57559b0afcb74a6c1426194415c094338d97a13c
Jonathan <jperalgo@sfu.ca>
2021-10-27 10:36:06 -0700

    📝Finished Machine Learning slides' notes

--- Exercises/6/e6/ab_analysis.py
+++ Exercises/6/e6/ab_analysis.py
@@ -1,7 +1,36 @@
 import pandas as pd
-import statsmodels
+from scipy import stats
 import numpy as np
 
 searches = pd.read_json('searches.json', orient='records', lines=True)
+
+
+global search_count
+search_count = 0
+
+def count(row):
+    global search_count
+    if(row['search_count'] > 0):
+        row['search_count'] = 'searched at least once'
+        search_count = search_count + 1
+    else:
+        row['search_count'] = 'not searched'
+
+    if(row['uid'] % 2 == 0):
+        row['uid'] = 'even'
+    else:
+        row['uid'] = 'odd'
+
+    return row
+
+searches = searches.apply(lambda x: count(x), axis=1)
+
+did_not_search_count = len(searches) - search_count
+print('Users that used search:', search_count, '\nUsers that did not use search:', did_not_search_count)
+
+contingency = []
+chi2, p, dof, expected = stats.chi2_contingency(contingency )
+print(p)
+print(expected)
 
 print(searches)
--- Exercises/Notes/machine_learning_notes_2.py
+++ Exercises/Notes/machine_learning_notes_2.py
@@ -21,6 +21,7 @@ model.fit(X_with, y)
 
 #Polynomial Regression
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
 
 poly = PolynomialFeatures(degree=3, include_bias=True)
 
@@ -33,4 +34,76 @@ model = LinearRegression(fit_intercept=False)
 model.fit(X_poly, y)
 
 print(model.coef_)
-#
+
+#Making a pipeline model
+model = make_pipeline(
+    PolynomialFeatures(degree=3, include_bias=True),
+    LinearRegression(fit_intercept=False)
+) #This model takes our x values, creates polynomial features, and does a linear fit on them
+
+y = [2, 3, 4, 3]
+y = np.stack([y], axis=1)
+
+model.fit(X, y)
+plt.plot(X_poly[:, 0], model.predict(X_poly), 'r.-')
+# plt.show()
+
+#Pipeline example:
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.impute import SimpleImputer
+
+model = make_pipeline(
+    SimpleImputer(strategy='mean'),     #input missing values
+    MinMaxScaler(),     #scale each feature to 0-1
+    PolynomialFeatures(degree=3, include_bias=True),
+    LinearRegression(fit_intercept=False)
+)
+
+#Creating a custom basis function for linear regression using pipeline model
+def sigmoid_basis(X):  # [[x]] -> [[1, x, 1/(1 + e^x)]]
+    one = np.ones(X.shape)
+    sigmoid = 1 / (1 + np.exp(X))
+    return np.concatenate((one, X, sigmoid), axis=1)
+
+from sklearn.preprocessing import FunctionTransformer
+model = make_pipeline(
+    FunctionTransformer(sigmoid_basis, validate=True),
+    LinearRegression(fit_intercept=False)
+)
+
+model.fit(X, y)
+print(model.named_steps['linearregression'].coef_)
+# plt.plot(model.predict(X_poly), 'r-')
+
+###
+###Splitting data
+###
+from sklearn.model_selection import train_test_split
+
+#data set
+x = [15, 2, -19, 5, 202, 22, 1, 3, 4, 5, 6, 7, 33, 10, 2, 200, 32, 12]
+y = [2, 3, 4, 3, 3 ,4 ,5 , 5, 10, 2, -1, 22, 134, 22, 55, -1, 0, 10]
+
+X = np.stack([x], axis=1)
+y = np.stack([y], axis=1)
+
+X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+
+def score_polyfit(n):
+    model = make_pipeline(
+        PolynomialFeatures(degree=n, include_bias=True),
+        LinearRegression(fit_intercept=False)
+    )
+    model.fit(X_train, y_train)
+    print('n=%i: score(train)=%.5g, score(valid)=%.5g' % (n, model.score(X_train, y_train), model.score(X_valid, y_valid)))
+    plt.plot(X_train, y_train, 'r-')
+    plt.plot(X_train[:, 0], model.predict(X_train), 'b.')
+    plt.plot(X_valid[:, 0], model.predict(X_valid), 'g.')
+    plt.show()
+
+print()
+score_polyfit(1)
+score_polyfit(5)
+score_polyfit(9)
+score_polyfit(13)
+score_polyfit(17)
