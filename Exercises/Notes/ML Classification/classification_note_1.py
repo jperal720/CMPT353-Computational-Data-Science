1108d887fabdc338c6659ec56cc3cbb560b415e2
jperalgo <jperalgo@sfu.ca>
2021-10-28 02:40:11 -0700

    Added some examples to Classifier notes

---
+++ Exercises/Notes/ML Classification/classification_note_1.py
@@ -0,0 +1,33 @@
+"""
+There are many ways to model classifications, all follow a similar and basic procedure:
+
+model = SomeClassifierModel(...)
+model.fit(X_train, y_train)
+y_predicted = model.predict(X_test)
+somehow_compare_results = (y_test, y_predicted)
+"""
+
+from sklearn.naive_bayes import GaussianNB
+
+X_train, X_valid = [] #Fill data set
+y_train, y_valid = [] #Fill data set
+
+model = GaussianNB()
+model.fit(X_train, y_train)
+
+y_predicted = model.predict(y_valid)
+
+print(model.theta_) #Sample means for each category in each dimension
+print(model.sigma_) #Sample variances for each category in each dimension
+
+from sklearn.metrics import classification_report
+print(classification_report(y_valid, y_predicted))   #Will display a classification report, precision can be found here
+
+"""
+Example of creating a Prior:
+
+model = GaussaianNB(priors=[1/7, 3/7, 3/7]) #By default, Priors are calculated from training data, so unless we know more than that, specifying explicitly won't help
+model.fit(X_train, y_train)
+print(model.score(X_valid, y_valid)
+
+"""
